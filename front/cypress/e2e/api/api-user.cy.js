d38bfdb36a8e2556a4dbe2bb10c0305ee19d09a0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-28 21:54:18 -0300

    feat: variaveis de ambiente e testes por viewport

--- front/cypress/e2e/api/api-user.cy.js
+++ front/cypress/e2e/api/api-user.cy.js
@@ -49,4 +49,19 @@ describe("Realizando requesições", () => {
             cy.getByData("titulo-boas-vindas").should("contain.text", "Bem vindo de volta!")
         })
     })
+
+    context('Realizando login via API', () => {
+        it('Deve permitir o login do usuário Neilton Seguins', () => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:8000/users/login',
+                body: Cypress.env(),
+            }).then((resposta) => {
+                expect(resposta.status).to.eq(200);
+                expect(resposta.body).is.not.empty;
+                expect(resposta.body.user).to.have.property('nome');
+                expect(resposta.body.user.nome).to.be.equal('Neilton Seguins');
+            });
+        });
+    });
 })
